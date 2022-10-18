eb8903127df03207e3ae33559ea89691f171e4d0
Haringey Fixers <97034764+haringeyfixers@users.noreply.github.com>
2022-10-18 21:53:28 +0100

    Create rdhf1.js

---
+++ docs/js/rdhf1.js
@@ -0,0 +1,47 @@
+var sw_targ=true; var ff_targ=''; var qs= querySt('r'); switch(qs) {
+case '122'	:ff_targ_text='Haringey Fixers';  ff_targ='https://hfixers.netlify.app?r122'; break; 
+        // bit.ly/XXXX 3ghYES7               https://haringeyfixers.github.io/redir/rdhfqr.htm?r=122	
+case '123'	:ff_targ_text='Haringey Fixers';  ff_targ='https://hfixers.netlify.app?r123'; break; 
+        // bit.ly/XXXX 3MFXuf9               https://haringeyfixers.github.io/redir/rdhfqr.htm?r=123
+case '124'	:ff_targ_text='Haringey Fixers';  ff_targ='https://hfixers.netlify.app?r124'; break;  
+        // bit.ly/XXXX 3EXW5yG               https://haringeyfixers.github.io/redir/rdhfqr.htm?r=124					
+case '125'	:ff_targ_text='Haringey Fixers';  ff_targ='https://hfixers.netlify.app?r125'; break; 
+        // bit.ly/XXXX 3ETv2Vk               https://haringeyfixers.github.io/redir/rdhfqr.htm?r=125					
+case '126'	:ff_targ_text='Haringey Fixers';  ff_targ='https://hfixers.netlify.app?r126'; break;  
+        // bit.ly/XXXX 3EM8mq9               https://haringeyfixers.github.io/redir/rdhfqr.htm?r=126					
+case '127'	:ff_targ_text='Haringey Fixers';  ff_targ='https://hfixers.netlify.app?r127'; break; 
+        // bit.ly/XXXX 3VCLJtY               https://haringeyfixers.github.io/redir/rdhfqr.htm?r=127					
+case '128'	:ff_targ_text='Haringey Fixers';  ff_targ='https://hfixers.netlify.app?r128'; break; 
+        // bit.ly/XXXX 3EPEfy4               https://haringeyfixers.github.io/redir/rdhfqr.htm?r=128					
+case '129'	:ff_targ_text='Haringey Fixers';  ff_targ='https://hfixers.netlify.app?r129'; break; 
+        // bit.ly/XXXX 3yNR0p2               https://haringeyfixers.github.io/redir/rdhfqr.htm?r=129					
+case '130'	:ff_targ_text='Haringey Fixers';  ff_targ='https://hfixers.netlify.app?r130'; break;  
+        // bit.ly/XXXX 3D8c9gk               https://haringeyfixers.github.io/redir/rdhfqr.htm?r=130					
+case '131'	:ff_targ_text='Haringey Fixers';  ff_targ='https://hfixers.netlify.app?r131'; break;  
+        // bit.ly/XXXX 3TkYJTk               https://haringeyfixers.github.io/redir/rdhfqr.htm?r=131					
+case '132'	:ff_targ_text='Haringey Fixers';  ff_targ='https://hfixers.netlify.app?r132'; break; 	
+       // bit.ly/XXXX 3MCGmXP                https://haringeyfixers.github.io/redir/rdhfqr.htm?r=132					
+case '133'	:ff_targ_text='Haringey Fixers';  ff_targ='https://hfixers.netlify.app?r133'; break; 
+        // bit.ly/XXXX 3yPIw0K               https://haringeyfixers.github.io/redir/rdhfqr.htm?r=133					
+case '134'	:ff_targ_text='Haringey Fixers';  ff_targ='https://hfixers.netlify.app?r134'; break;  
+        // bit.ly/XXXX 3VDLgaX               https://haringeyfixers.github.io/redir/rdhfqr.htm?r=134					
+case '135'	:ff_targ_text='Haringey Fixers';  ff_targ='https://hfixers.netlify.app?r135'; break; 
+        // bit.ly/XXXX 3yMBidG               https://haringeyfixers.github.io/redir/rdhfqr.htm?r=135					
+case '136'	:ff_targ_text='Haringey Fixers';  ff_targ='https://hfixers.netlify.app?r136'; break; 
+        // bit.ly/XXXX 3VACZEW               https://haringeyfixers.github.io/redir/rdhfqr.htm?r=136					
+case '137'	:ff_targ_text='Haringey Fixers';  ff_targ='https://hfixers.netlify.app?r137'; break; 
+        // bit.ly/XXXX 3yQpA1z               https://haringeyfixers.github.io/redir/rdhfqr.htm?r=137					
+case '138'	:ff_targ_text='Haringey Fixers';  ff_targ='https://hfixers.netlify.app?r138'; break; 
+        // bit.ly/XXXX 3s6gYQQ               https://haringeyfixers.github.io/redir/rdhfqr.htm?r=138					
+case '139'	:ff_targ_text='Haringey Fixers';  ff_targ='https://hfixers.netlify.app?r139'; break; 
+        // bit.ly/XXXX 3s6hiiw               https://haringeyfixers.github.io/redir/rdhfqr.htm?r=139					
+case '140'  :ff_targ_text='Haringey Fixers';  ff_targ='https://hfixers.netlify.app?r140'; break; 
+        // bit.ly/XXXX 3Tcndyx               https://haringeyfixers.github.io/redir/rdhfqr.htm?r=140									
+
+default:sw_targ=false;break;
+}
+if (sw_targ==true) {document.write('<meta http-equiv="refresh" content="1;url='+ff_targ+'"><title>'+ff_targ_text+'</title>');
+                   window.location.href=ff_targ;}
+else {document.write('There may have been an error - no action possible:'+ff_targ);}
+function querySt(Key) {var url = window.location.href; KeysValues = url.split(/[\?&]+/);
+for (i = 0; i < KeysValues.length; i++) {KeyValue = KeysValues[i].split("="); if (KeyValue[0] == Key) {return KeyValue[1];}}}
