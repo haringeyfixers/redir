d1428a5e7510c68705a691b695d2a22b264471ca
Haringey Fixers <97034764+haringeyfixers@users.noreply.github.com>
2022-10-31 20:30:25 +0000

    Update rdhfqr.js

--- docs/js/rdhfqr.js
+++ docs/js/rdhfqr.js
@@ -42,6 +42,6 @@ default:sw_targ=false;break;
 }
 if (sw_targ==true) {document.write('<meta http-equiv="refresh" content="1;url='+ff_targ+'"><title>'+ff_targ_text+'</title>');
                    window.location.href=ff_targ;}
-else {document.write('There may have been an error - no action possible, qs->'qs+'<- targ->'+ff_targ + '<-');}
+else {document.write('There may have been an error - no action possible, qs->'+qs+ '<- targ->' +ff_targ+ '<-');}
 function querySt(Key) {var url = window.location.href; KeysValues = url.split(/[\?&]+/);
 for (i = 0; i < KeysValues.length; i++) {KeyValue = KeysValues[i].split("="); if (KeyValue[0] == Key) {return KeyValue[1];}}}
