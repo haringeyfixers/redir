001c0f47bf4274050b65da6e5260c94f2c53c833
Haringey Fixers <97034764+haringeyfixers@users.noreply.github.com>
2022-11-01 09:39:41 +0000

    Update rdhfdx.js

--- docs/js/rdhfdx.js
+++ docs/js/rdhfdx.js
@@ -1,5 +1,5 @@
 var sw_targ=true, ff_targ='', qs= querySt('x'), szx=qs.substring(1);
-if (len(szx)>0) {ff_targ='https://hfixers.netlify.app?x='+szx;} else {sw_targ=false;}
+if (szx.length > 0) {ff_targ='https://hfixers.netlify.app?x='+szx;} else {sw_targ=false;}
 // catchall redir https://haringeyfixers.github.io/redir/rdhfdx.htm?x=100	
 if (sw_targ==true) {document.write('<meta http-equiv="refresh" content="1;url='+ff_targ+'"><title>'+ff_targ_text+'</title>');			
 window.location.href=ff_targ;}			
