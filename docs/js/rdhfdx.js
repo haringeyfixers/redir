77dd77a513d8d40129be3c1513014ca432be702a
Haringey Fixers <97034764+haringeyfixers@users.noreply.github.com>
2022-11-01 18:59:51 +0000

    Update rdhfdx.js

--- docs/js/rdhfdx.js
+++ docs/js/rdhfdx.js
@@ -1,6 +1,9 @@
-var sw_targ=true, ff_targ='', ff_targ_text='Haringey Fixers', qs= querySt('x');
-if (qs.length > 0) {ff_targ='https://hfixers.netlify.app?x='+qs;} else {sw_targ=false;}
+var sw_targ=true, ff_targ='', ff_targ_text='Haringey Fixers', qs= querySt('x'); if (qs.length < 1) {sw_targ=false;}
 // catchall redir https://haringeyfixers.github.io/redir/rdhfdx.htm?x=100	
+switch(qs) {
+case '100'	:ff_targ_text='BroadWater Farm';  		ff_targ='https://fb.com/groups/BroadWaterFarm'; break;		
+default:     ff_targ_text='Haringey Fixers';  		ff_targ='https://hfixers.netlify.app?x='+qs;    break;					
+}    
 if (sw_targ==true) {document.write('<meta http-equiv="refresh" content="1;url='+ff_targ+'"><title>'+ff_targ_text+'</title>');			
 window.location.href=ff_targ;}			
 else {document.write('There may have been an error - no action possible, qs->'+qs+ '<- targ->' +ff_targ+ '<-');}			
