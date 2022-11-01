576978802922ee2234ef7407807d094d40ca39e4
Haringey Fixers <97034764+haringeyfixers@users.noreply.github.com>
2022-11-01 09:35:40 +0000

    Create rdhfdx.js

---
+++ docs/js/rdhfdx.js
@@ -0,0 +1,8 @@
+var sw_targ=true, ff_targ='', qs= querySt('x'), szx=qs.substring(1);
+if (len(szx)>0) {ff_targ='https://hfixers.netlify.app?x='+qs;} else {sw_targ=false;}
+
+if (sw_targ==true) {document.write('<meta http-equiv="refresh" content="1;url='+ff_targ+'"><title>'+ff_targ_text+'</title>');			
+window.location.href=ff_targ;}			
+else {document.write('There may have been an error - no action possible, qs->'+qs+ '<- targ->' +ff_targ+ '<-');}			
+function querySt(Key) {var url = window.location.href; KeysValues = url.split(/[\?&]+/);			
+for (i = 0; i < KeysValues.length; i++) {KeyValue = KeysValues[i].split("="); if (KeyValue[0] == Key) {return KeyValue[1];}}}
