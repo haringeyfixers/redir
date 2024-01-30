ba1c090fae7186fb0442a0d6f1bea60a2168924b
Haringey Fixers <97034764+haringeyfixers@users.noreply.github.com>
2024-01-30 09:42:12 +0000

    Update rdhsc1.js

--- docs/js/rdhsc1.js
+++ docs/js/rdhsc1.js
@@ -3,7 +3,10 @@ case '122'	:ff_targ_text='Haringey Fixers';  ff_targ='https://hfixers.netlify.ap
         // bit.ly/rdhsc1?r=122&z=	
 case '123'	:ff_targ_text='BWF Social Cinema';  ff_targ='https://www.eventbrite.co.uk/e/539233310347/'; break; 
         // bit.ly/bwfs3 3FfHezg               https://haringeyfixers.github.io/redir/rdhsc1.htm?r=123
-
+case '124'	:ff_targ_text='HF Recruitment & Selection Privacy Notice';  ff_targ='https://docs.google.com/document/d/1gxXAEgHrHH3KykCOPeEzrM54t88c19anuccEwwcRxqs/edit#heading=h.7f4a0ddu8wu1'; break; 
+        // bit.ly/hfpolrspn                https://haringeyfixers.github.io/redir/rdhsc1.htm?r=124
+case '125'	:ff_targ_text='spare already bitlyd';  ff_targ='spare'; break; 
+        // bit.ly/                https://haringeyfixers.github.io/redir/rdhsc1.htm?r=125                
 default:sw_targ=false;break;
 }
 if (sw_targ==true) {document.write('<meta http-equiv="refresh" content="1;url='+ff_targ+'"><title>'+ff_targ_text+'</title>');
