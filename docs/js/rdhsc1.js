6dfc888daef2530fdbc4ea9b0bdf981aa829e495
Haringey Fixers <97034764+haringeyfixers@users.noreply.github.com>
2024-01-30 09:54:37 +0000

    Update rdhsc1.js

--- docs/js/rdhsc1.js
+++ docs/js/rdhsc1.js
@@ -1,10 +1,10 @@
 var sw_targ=true; var ff_targ=''; var qs= querySt('r'); switch(qs) {
 case '122'	:ff_targ_text='Haringey Fixers';  ff_targ='https://hfixers.netlify.app?r122'; break; 
-        // bit.ly/rdhsc1?r=122&z=	
+        // https://bit.ly/rdhsc1?r=122&z=	
 case '123'	:ff_targ_text='BWF Social Cinema';  ff_targ='https://www.eventbrite.co.uk/e/539233310347/'; break; 
-        // bit.ly/bwfs3 3FfHezg               https://haringeyfixers.github.io/redir/rdhsc1.htm?r=123
+        // https://bit.ly/bwfs3 3FfHezg               https://haringeyfixers.github.io/redir/rdhsc1.htm?r=123
 case '124'	:ff_targ_text='HF Recruitment & Selection Privacy Notice';  ff_targ='https://docs.google.com/document/d/1gxXAEgHrHH3KykCOPeEzrM54t88c19anuccEwwcRxqs/edit#heading=h.7f4a0ddu8wu1'; break; 
-        // bit.ly/hfpolrspn                https://haringeyfixers.github.io/redir/rdhsc1.htm?r=124
+        // https://bit.ly/hfpolrspn                https://haringeyfixers.github.io/redir/rdhsc1.htm?r=124
 case '125'	:ff_targ_text='spare already bitlyd';  ff_targ='spare'; break; 
         // bit.ly/                https://haringeyfixers.github.io/redir/rdhsc1.htm?r=125                
 default:sw_targ=false;break;
