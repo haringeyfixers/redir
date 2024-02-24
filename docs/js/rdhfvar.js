5afa8ef4d69f39eb68e17e1d6d289705b8759791
Haringey Fixers <97034764+haringeyfixers@users.noreply.github.com>
2024-02-24 18:39:56 +0000

    Update rdhfvar.js

--- docs/js/rdhfvar.js
+++ docs/js/rdhfvar.js
@@ -1,14 +1,24 @@
 var sw_targ=true; let ff_targ='', qs= querySt('r'); 
 switch(qs) {
-  case 'rcag'	: let vqs= querySt('v'), vqs_text='All Good Repair Café', vqs_dest='';
-                switch(vqs) {
-                  case '22'	: ff_targ_text='xxxx';  ff_targ='xxxxxxxxxx'; break; 
-                  // https://bit.ly/xxxx                https://haringeyfixers.github.io/redir/rdhfvar.htm?r=rcag#v=22 
-                  case '23'	: ff_targ_text='xxxx';  ff_targ='xxxxxxxxxx'; break; 
-                  // https://bit.ly/xxxx                https://haringeyfixers.github.io/redir/rdhfvar.htm?r=rcag#v=23 
-                  default:sw_targ=false;break;
-               } 
-              break;
+  case 'rcag'	: let vqs= querySt('v'); console.log("vqs="+vqs);
+    /* placeholders so each different bit.ly accumulates stats  
+    https://bit.ly/hfag22 https://haringeyfixers.github.io/redir/rdhfvar.htm?r=rcag#v=22 
+    https://bit.ly/hfag23 https://haringeyfixers.github.io/redir/rdhfvar.htm?r=rcag#v=23 
+    https://bit.ly/hfag24 https://haringeyfixers.github.io/redir/rdhfvar.htm?r=rcag#v=24 
+    https://bit.ly/hfag25 https://haringeyfixers.github.io/redir/rdhfvar.htm?r=rcag#v=25 
+    https://bit.ly/hfag26 https://haringeyfixers.github.io/redir/rdhfvar.htm?r=rcag#v=26 
+    https://bit.ly/hfag27 https://haringeyfixers.github.io/redir/rdhfvar.htm?r=rcag#v=27 
+    https://bit.ly/hfag28 https://haringeyfixers.github.io/redir/rdhfvar.htm?r=rcag#v=28 
+    https://bit.ly/hfag29 https://haringeyfixers.github.io/redir/rdhfvar.htm?r=rcag#v=29 
+    https://bit.ly/hfag32 https://haringeyfixers.github.io/redir/rdhfvar.htm?r=rcag#v=32 
+    https://bit.ly/hfag33 https://haringeyfixers.github.io/redir/rdhfvar.htm?r=rcag#v=33 
+    https://bit.ly/hfag34 https://haringeyfixers.github.io/redir/rdhfvar.htm?r=rcag#v=34 
+    https://bit.ly/hfag35 https://haringeyfixers.github.io/redir/rdhfvar.htm?r=rcag#v=35 
+    https://bit.ly/hfag36 https://haringeyfixers.github.io/redir/rdhfvar.htm?r=rcag#v=36 
+    https://bit.ly/hfag37 https://haringeyfixers.github.io/redir/rdhfvar.htm?r=rcag#v=37 
+    https://bit.ly/hfag38 https://haringeyfixers.github.io/redir/rdhfvar.htm?r=rcag#v=38 */    
+    let ff_targ_text='All Good Repair Café', ff_targ='https://www.eventbrite.com/cc/haringey-all-good-repair-cafes-turnpike-lane-3116259';
+    break;
   default:sw_targ=false;break;
 }
 if (sw_targ==true) {document.write('<meta http-equiv="refresh" content="1;url='+ff_targ+'"><title>'+ff_targ_text+'</title>');
