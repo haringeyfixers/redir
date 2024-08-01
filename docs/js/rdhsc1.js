8cf0d9c436bbfdee9a2e90221498e23126f9f75a
Haringey Fixers <97034764+haringeyfixers@users.noreply.github.com>
2024-08-01 12:16:13 +0100

    Update rdhsc1.js

--- docs/js/rdhsc1.js
+++ docs/js/rdhsc1.js
@@ -1,25 +1,36 @@
-var sw_targ=true; var ff_targ=''; var qs= querySt('r'); switch(qs) {
-case '122'	:ff_targ_text='Haringey Fixers';  ff_targ='https://hfixers.netlify.app?r122'; break; 
-        // https://bit.ly/rdhsc1?r=122&z=	
-case '123'	:ff_targ_text='BWF Social Cinema';  ff_targ='https://www.eventbrite.co.uk/e/539233310347/'; break; 
-        // https://bit.ly/bwfs3 3FfHezg               https://haringeyfixers.github.io/redir/rdhsc1.htm?r=123
-case '124'	:ff_targ_text='HF Recruitment & Selection Privacy Notice';  ff_targ='https://docs.google.com/document/d/1gxXAEgHrHH3KykCOPeEzrM54t88c19anuccEwwcRxqs/edit#heading=h.7f4a0ddu8wu1'; break; 
-        // https://bit.ly/hfpolrspn                https://haringeyfixers.github.io/redir/rdhsc1.htm?r=124
-case '125'	:ff_targ_text='Work With Us';  ff_targ='https://haringeyfixers.org/about-us/work-with-us/'; break; 
-        // https://bit.ly/hfworkwith                https://haringeyfixers.github.io/redir/rdhsc1.htm?r=125     
-case '126'	:ff_targ_text='Google Meet 2024';  ff_targ='https://meet.google.com/una-attb-nao'; break; 
-        // https://bit.ly/hfgmeet                https://haringeyfixers.github.io/redir/rdhsc1.htm?r=126    
-case '127'	:ff_targ_text='xxxx';  ff_targ='xxxxxxxxxx'; break; 
-        // https://bit.ly/xxxx                https://haringeyfixers.github.io/redir/rdhsc1.htm?r=127  
-case '128'	:ff_targ_text='xxxx';  ff_targ='xxxxxxxxxx'; break; 
-        // https://bit.ly/xxxx                https://haringeyfixers.github.io/redir/rdhsc1.htm?r=128  
-case '129'	:ff_targ_text='xxxx';  ff_targ='xxxxxxxxxx'; break; 
-        // https://bit.ly/xxxx                https://haringeyfixers.github.io/redir/rdhsc1.htm?r=129  
-        
-default:sw_targ=false;break;
-}
-if (sw_targ==true) {document.write('<meta http-equiv="refresh" content="1;url='+ff_targ+'"><title>'+ff_targ_text+'</title>');
-                   window.location.href=ff_targ;}
-else {document.write('There may have been an error - no action possible:'+ff_targ);}
-function querySt(Key) {var url = window.location.href; KeysValues = url.split(/[\?&]+/);
-for (i = 0; i < KeysValues.length; i++) {KeyValue = KeysValues[i].split("="); if (KeyValue[0] == Key) {return KeyValue[1];}}}
+var sw_targ=true; var ff_targ=''; var qs= querySt('r'); switch(qs) {									
+case '122'	:ff_targ_text='Haringey Fixers';  ff_targ='https://hfixers.netlify.app?r122'; break;								
+// https://bit.ly/rdhsc1?r=122&z=									
+case '123'	:ff_targ_text='BWF Social Cinema';  ff_targ='https://www.eventbrite.co.uk/e/539233310347/'; break;								
+// https://bit.ly/bwfs3 3FfHezg               https://haringeyfixers.github.io/redir/rdhsc1.htm?r=123									
+case '124'	:ff_targ_text='HF Recruitment & Selection Privacy Notice';  ff_targ='https://docs.google.com/document/d/1gxXAEgHrHH3KykCOPeEzrM54t88c19anuccEwwcRxqs/edit#heading=h.7f4a0ddu8wu1'; break;								
+// https://bit.ly/hfpolrspn                https://haringeyfixers.github.io/redir/rdhsc1.htm?r=124									
+case '125'	:ff_targ_text='Work With Us';  ff_targ='https://haringeyfixers.org/about-us/work-with-us/'; break;								
+// https://bit.ly/hfworkwith                https://haringeyfixers.github.io/redir/rdhsc1.htm?r=125									
+case '126'	:ff_targ_text='Google Meet 2024';  ff_targ='https://meet.google.com/una-attb-nao'; break;								
+// https://bit.ly/hfgmeet                https://haringeyfixers.github.io/redir/rdhsc1.htm?r=126									
+case '127'	:ff_targ_text='HF Bringer Broken Item';  ff_targ='https://forms.gle/HN8njSdFt3JmumxV8'; break;								
+// https://bit.ly/hfrc27              https://haringeyfixers.github.io/redir/rdhsc1.htm?r=127									
+case '128'	:ff_targ_text='HF New Volunteer Form';  ff_targ='https://forms.gle/VqW7Z7eRLeLaj4ow7'; break;								
+// https://bit.ly/hfrc28a             https://haringeyfixers.github.io/redir/rdhsc1.htm?r=128									
+case '129'	:ff_targ_text='HFRC Disclaimer in Person';  ff_targ='https://docs.google.com/document/d/1lJ1vQxCHFlJIIcWtddHkQ_RyGRiO4jJ6jj3J5sVFw_E/edit?usp=sharing'; break;								
+// https://bit.ly/hfrc29          https://haringeyfixers.github.io/redir/rdhsc1.htm?r=129									
+case '130'	:ff_targ_text='HFRC Disclaimer in Person';  ff_targ='https://docs.google.com/document/d/1lJ1vQxCHFlJIIcWtddHkQ_RyGRiO4jJ6jj3J5sVFw_E/edit?usp=sharing'; break;								
+// https://bit.ly/hfrc29          https://haringeyfixers.github.io/redir/rdhsc1.htm?r=130
+case '131'	:ff_targ_text='HFRC Disclaimer in Person';  ff_targ='https://docs.google.com/document/d/1lJ1vQxCHFlJIIcWtddHkQ_RyGRiO4jJ6jj3J5sVFw_E/edit?usp=sharing'; break;								
+// https://bit.ly/hfrc29          https://haringeyfixers.github.io/redir/rdhsc1.htm?r=131
+case '132'	:ff_targ_text='HFRC Disclaimer in Person';  ff_targ='https://docs.google.com/document/d/1lJ1vQxCHFlJIIcWtddHkQ_RyGRiO4jJ6jj3J5sVFw_E/edit?usp=sharing'; break;								
+// https://bit.ly/hfrc29          https://haringeyfixers.github.io/redir/rdhsc1.htm?r=132
+case '133'	:ff_targ_text='HFRC Disclaimer in Person';  ff_targ='https://docs.google.com/document/d/1lJ1vQxCHFlJIIcWtddHkQ_RyGRiO4jJ6jj3J5sVFw_E/edit?usp=sharing'; break;								
+// https://bit.ly/hfrc29          https://haringeyfixers.github.io/redir/rdhsc1.htm?r=133
+case '134'	:ff_targ_text='HFRC Disclaimer in Person';  ff_targ='https://docs.google.com/document/d/1lJ1vQxCHFlJIIcWtddHkQ_RyGRiO4jJ6jj3J5sVFw_E/edit?usp=sharing'; break;								
+// https://bit.ly/hfrc29          https://haringeyfixers.github.io/redir/rdhsc1.htm?r=134
+
+									
+default:sw_targ=false;break;									
+}									
+if (sw_targ==true) {document.write('<meta http-equiv="refresh" content="1;url='+ff_targ+'"><title>'+ff_targ_text+'</title>');									
+window.location.href=ff_targ;}									
+else {document.write('There may have been an error - no action possible:'+ff_targ);}									
+function querySt(Key) {var url = window.location.href; KeysValues = url.split(/[\?&]+/);									
+for (i = 0; i < KeysValues.length; i++) {KeyValue = KeysValues[i].split("="); if (KeyValue[0] == Key) {return KeyValue[1];}}}	
