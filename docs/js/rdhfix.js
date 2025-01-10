5a5d635832249f9cfa99c90ff74af3b043fdea67
Chris <zawsx@users.noreply.github.com>
2025-01-10 18:45:04 +0000

    Update rdhfix.js

--- docs/js/rdhfix.js
+++ docs/js/rdhfix.js
@@ -18,7 +18,7 @@ case 'f22'	:ff_targ_text='Haringey Fixers';  ff_targ='https://HaringeyFixers.org
                 // bit.ly/HaringeyFixers https://haringeyfixers.github.io/redir/rdhfix.htm?h=f22	
 case 'f23'	:ff_targ_text='Haringey Fixers Overview'; ff_targ='https://docs.google.com/spreadsheets/d/1LaCUWUF886CihC-RcnagpLZMLllJtATr6qsTUHKg3Ss/edit?usp=sharing';break;
                 // http://bit.ly/hfixovw https://haringeyfixers.github.io/redir/rdhfix.htm?h=f23
-case 'f24'	:ff_targ_text='HfixAdmin';  ff_targ='https://docs.google.com/spreadsheets/d/1PeSyLKmi4tophgGlAk_ibcVGnw87pKLqYq3bexHNx34/edit?gid=772207304#gid=772207304&range=A1';break; 
+case 'f24'	:ff_targ_text='HfixAdmin';  ff_targ='https://docs.google.com/spreadsheets/d/e/2PACX-1vRFjxsno6VUit-C8SIWAI7Yrv1AYzVwo330yAZChROggf9QXWIMu58nV4_sJ8CqL8raCk2l-6Z5h--s/pubhtml?gid=772207304&single=true';break; 
         // bit.ly/hfixadmin https://haringeyfixers.github.io/redir/rdhfix.htm?h=f24					
 case 'f25'	:ff_targ_text='Hfix booking - Disclaimer'; ff_targ='https://docs.google.com/forms/d/e/1FAIpQLSdWSjWvS4EhvsDq8tSPRbi0LrPcZ8lyB1FufprsNvT9KyX6pg/viewform?usp=sf_link';break; 
         // bit.ly/HfixBookDisclaim https://haringeyfixers.github.io/redir/rdhfix.htm?h=f25					
